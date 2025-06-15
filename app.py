91c58617b5ec97d38cb8c6101f3893478fdf1987
Gajendra L <100460060+Gajaindra@users.noreply.github.com>
2025-06-15 08:27:41 +0530

    app.py

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ def index():
 
     return render_template("index.html", answer=answer)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
+
